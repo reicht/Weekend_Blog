7170f0f141350d75f2318ca48bcd9aae8c3b44ec
Jordan Corley <rainwolfj@gmail.com>
2016-03-24 15:10:35 -0500

    Seed file introduced, will use for simulating past inputs and future storage.  Next step is getting simulated history to exist so I can try to view it.

---
+++ db/seed.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/all'
+
+$post_ids = 0
+
+$comment_ids = 0
+
+$starter_posts = ["Words", "And", "Things"]
+
+$starter_comments = ["Stuff", "About", "Penguins"]
+
+$post_list = []
+
+$comment_list = {}
