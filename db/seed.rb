880f2e24b9e89430a88d33f1b203946a70b9f16c
Jordan Corley <rainwolfj@gmail.com>
2016-03-25 09:42:55 -0500

    Morning work starting point, going to retrieve the base updates and try to fold them in

--- db/seed.rb
+++ db/seed.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/all'
+# require_relative '../lib/all'
 
 $post_ids = 0
 
@@ -6,12 +6,14 @@ $comment_ids = 0
 
 $starter_post_titles = ["Words", "And", "Things"]
 
-$starter_post_titles = ["Cheetah, Hyena, Shingles, Roofing, Piano, Separatists, Celebrated, Rockets, Gleaming, Waves",
+$starter_post_contents = ["Cheetah, Hyena, Shingles, Roofing, Piano, Separatists, Celebrated, Rockets, Gleaming, Waves",
                         "And, then, until, except, suddenly, although, but, therefore",
                         "Things are amazing because they are, if things aren't, then the thing isn't a thus isn't a thing.",
                       ]
 
-$starter_comments = ["Stuff", "Connections", "Penguins"]
+$starter_comments = [["Animals, Summer Work, Musical, Fourth of July, Ocean", "Connections", "Penguins should be here"],
+                    ["What?", "This seriously makes no sense"],
+                    ]
 
 $post_list = []
 
